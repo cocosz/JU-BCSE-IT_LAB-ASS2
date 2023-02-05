fa8503f16f26e7ee17b06aeeaf035f4bca3d098e
Tanvir Alam <93071696+cocosz@users.noreply.github.com>
2023-02-06 00:51:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// Registration and Login with Password Hash
+import express from 'express'
+import connectDB from './db/connectdb.js'
+import bodyparser from 'body-parser'
+import web from './routes/web.js'
+import path from 'path'
+import __dirname from './paths.js'
+import session from 'express-session'
+const app = express()
+// const session= require('express-session')
+const port = process.env.PORT || '3000'
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
+
+// Database Connection
+connectDB(DATABASE_URL)
+
+// Set Template Engine
+app.set('view engine', 'ejs')
+app.use(session({secret:"myPassword",
+resave: false,
+saveUninitialized: false
+}))
+app.use(express.static(path.join(__dirname, "static")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+
+// Load Routes
+app.use('/', web)
+
+app.listen(port, () => {
+ console.log(`Server listening at http://localhost:${port}`)
+})
